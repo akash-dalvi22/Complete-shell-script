a21313db0c0dff29d7725dfe3e0c8b1c79c47d1f
akash-dalvi22 <akashdalvi341@gmail.com>
2025-11-15 22:24:12 +0530

    This is the first commit

---
+++ Script1.sh
@@ -0,0 +1,4 @@
+echo "We are trying this practical from linux EC2"
+echo "I am from NextPath DevOps" 
+echo "I am learning Git/Github"
+echo "I have added new code"
---
+++ Script6.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# test of few of the fixed variables
+
+echo "script name: $0"
+echo "1st cmdla: $1"
+echo "2nd cmdla: $2"
+echo "cmdla list: $@"
+echo "no of cmdla: $#"
