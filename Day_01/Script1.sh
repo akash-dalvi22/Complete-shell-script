301270f66a4f96542e6a9c8742e8dc868cb6baf3
Akash Dalvi <akashdalvi341@gmail.com>
2026-07-29 23:18:12 +0530

    first day practical

---
+++ Day_01/Script1.sh
@@ -0,0 +1,7 @@
+
+# This Script prints some text to the console
+echo "Hello all, welcome to the DevOps session"
+echo "I am from NextPath DevOps" 
+echo "We are learning Shell scripting"
+
+# ./script1.sh --> to execute the script
---
+++ Day_01/Script2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#This script is to read input from the user/console
+echo "Enter your name"
+read name
+echo "Hello $name, welcome to the DevOps session"
