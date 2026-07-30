8fb7970aed2f900f5ed4af3699fb3e1a01299048
Akash Dalvi <akashdalvi341@gmail.com>
2026-07-30 22:36:50 +0530

    variable demonstration

---
+++ Day_02/Variable3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Using variables we can store the output of commands and reuse them later in the script. This can help make our scripts more efficient and easier to read.
+
+current_date=$(date)
+echo "Today's date is: $current_date"
