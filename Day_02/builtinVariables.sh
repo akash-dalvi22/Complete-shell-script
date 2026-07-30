7a8c109bedf90d9817c60e3fbc13d26c1e70573f
Akash Dalvi <akashdalvi341@gmail.com>
2026-07-30 22:57:38 +0530

    variable demonstration

---
+++ Day_02/builtinVariables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# We also have some built-in variables in bash that can be used to get information about the script and the environment it is running in. Some of these variables include:
+echo "Script name: $0"
+echo "First argument: $1"
+echo "Second argument: $2"
+echo "Number of arguments: $#"
+echo "All arguments: $@"
---
+++ Day_02/envVariables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script demonstrates the environment variables in bash. Environment variables are variables that are set by the operating system and can be accessed by any process running on the system. Some common environment variables include:
+
+echo "Home directory: $HOME"
+echo "Current working directory: $PWD"
+echo "User name: $USER"
+echo "Shell: $SHELL"
+echo "Path: $PATH"
