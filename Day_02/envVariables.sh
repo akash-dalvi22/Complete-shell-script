87c5d6eb416cbc5691b498639cc4ffcbe671643a
Akash Dalvi <akashdalvi341@gmail.com>
2026-08-07 00:03:55 +0530

    updating code

--- Day_02/envVariables.sh
+++ Day_02/envVariables.sh
@@ -7,3 +7,11 @@ echo "Current working directory: $PWD"
 echo "User name: $USER"
 echo "Shell: $SHELL"
 echo "Path: $PATH"
+
+
+#this is to check if the shell scripts picks up env variables
+echo "This is my system path $PATH"
+
+#Set a JDK_HOME env variable
+export JDK_HOME=/bin/jdk
+echo "my new JDK home is=$JDK_HOME"
---
+++ Day_02/readonly.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#This script is to make variable read only, means we cannot set the value of NAME variable again
+
+NAME=NextPath-Devops
+readonly NAME
+NAME=DEVOPS
+echo "my name is: $NAME"
